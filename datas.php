c3ffe4243bc8df6c3b0a1f48b65d53a7439e6fb1
陈红 <chenhong@xindong.com>
2015-07-30 17:40:22 +0800

    test

--- datas.php
+++ datas.php
@@ -1,89 +1,397 @@
 <?php
-if($_POST)
+$type = $_REQUEST['type'];
+
+switch($type)
+{
+    case 'detail':
+        get_detail();
+        break;
+    case 'detail2':
+        get_detail2();
+        break;
+    case 'all':
+        get_all();
+        break;
+    default:
+        echo json_encode([]);
+}
+
+function get_all()
 {
     header('Content-Type: application/json');
     $datas = [
-        [
-            'base' => [
-                [
-                    'name' => '收入',
-                    'value' => 100,
-                ],
-                [
-                    'name' => '日活跃',
-                    'value' => 200,
-                ],
-                [
-                    'name' => '付费用户',
-                    'value' => 50,
-                ],
-                [
-                    'name' => '付费率',
-                    'value' => '1%',
-                ],
-                [
-                    'name' => 'ARPPU',
-                    'value' => 54,
-                ],
-            ],
-            'remain' => [
-                [
-                    'name' => '次日留存',
-                    'value' => '50%',
-                ],
-                [
-                    'name' => '3日留存',
-                    'value' => '40%',
-                ],
-                [
-                    'name' => '4日留存',
-                    'value' => '30%',
-                ],
-            ],
+        'total' => [
+            [
+                'name' => '总收入',
+                'value' => 12345
+            ]
         ],
-        [
-            'base' => [
-                [
-                    'name' => '收入2',
-                    'value' => 100,
-                ],
-                [
-                    'name' => '日活跃2',
-                    'value' => 200,
-                ],
-                [
-                    'name' => '付费用户2',
-                    'value' => 50,
-                ],
-                [
-                    'name' => '付费率2',
-                    'value' => '1%',
-                ],
-                [
-                    'name' => 'ARPPU2',
-                    'value' => 54,
-                ],
+        'detail' => [
+            [
+                'game' => '口水三国',
+                'basedata' => [
+                    [
+                        'name' => '收入',
+                        'value' => 100,
+                    ],
+                    [
+                        'name' => '日活跃',
+                        'value' => 200,
+                    ],
+                    [
+                        'name' => '付费用户',
+                        'value' => 50,
+                    ],
+                    [
+                        'name' => '付费率',
+                        'value' => '1%',
+                    ],
+                    [
+                        'name' => 'ARPPU',
+                        'value' => 54,
+                    ],
+                ]
             ],
-            'remain' => [
-                [
-                    'name' => '次日留存2',
-                    'value' => '50%',
-                ],
-                [
-                    'name' => '3日留存2',
-                    'value' => '40%',
-                ],
-                [
-                    'name' => '4日留存2',
-                    'value' => '30%',
-                ],
-            ],
+            [
+                'game' => '塔塔塔防',
+                'basedata' => [
+                    [
+                        'name' => '收入',
+                        'value' => 100,
+                    ],
+                    [
+                        'name' => '日活跃',
+                        'value' => 200,
+                    ],
+                    [
+                        'name' => '付费用户',
+                        'value' => 50,
+                    ],
+                    [
+                        'name' => '付费率',
+                        'value' => '1%',
+                    ],
+                    [
+                        'name' => 'ARPPU',
+                        'value' => 54,
+                    ],
+                ]
+            ]
         ]
     ];
-    $time = $_POST['datetime'];
-    $k = date('d', $time) % 2;
-    $data = $datas[$k];
-    echo json_encode($data);
+    echo json_encode($datas);
+}
+
+function get_detail()
+{
+    if($_POST)
+    {
+        header('Content-Type: application/json');
+        $datas = [
+            [
+                'base' => [
+                    [
+                        'name' => '收入',
+                        'value' => 100,
+                    ],
+                    [
+                        'name' => '日活跃',
+                        'value' => 200,
+                    ],
+                    [
+                        'name' => '付费用户',
+                        'value' => 50,
+                    ],
+                    [
+                        'name' => '付费率',
+                        'value' => '1%',
+                    ],
+                    [
+                        'name' => 'ARPPU',
+                        'value' => 54,
+                    ],
+                ],
+                'remain' => [
+                    [
+                        'name' => '次日留存',
+                        'value' => '50%',
+                    ],
+                    [
+                        'name' => '3日留存',
+                        'value' => '40%',
+                    ],
+                    [
+                        'name' => '4日留存',
+                        'value' => '30%',
+                    ],
+                ],
+            ],
+            [
+                'base' => [
+                    [
+                        'name' => '收入2',
+                        'value' => 100,
+                    ],
+                    [
+                        'name' => '日活跃2',
+                        'value' => 200,
+                    ],
+                    [
+                        'name' => '付费用户2',
+                        'value' => 50,
+                    ],
+                    [
+                        'name' => '付费率2',
+                        'value' => '1%',
+                    ],
+                    [
+                        'name' => 'ARPPU2',
+                        'value' => 54,
+                    ],
+                ],
+                'remain' => [
+                    [
+                        'name' => '次日留存2',
+                        'value' => '50%',
+                    ],
+                    [
+                        'name' => '3日留存2',
+                        'value' => '40%',
+                    ],
+                    [
+                        'name' => '4日留存2',
+                        'value' => '30%',
+                    ],
+                ],
+            ]
+        ];
+        $time = $_POST['datetime'];
+        $k = date('d', $time) % 2;
+        $data = $datas[$k];
+        echo json_encode($data);
+    }
+}
+
+function get_detail2()
+{
+    if($_POST)
+    {
+        header('Content-Type: application/json');
+        $datas = [
+            [
+                'base' => [
+                    '2015-06-01' => [
+                        [
+                            'name' => '收入',
+                            'value' => 100,
+                        ],
+                        [
+                            'name' => '日活跃',
+                            'value' => 200,
+                        ],
+                        [
+                            'name' => '付费用户',
+                            'value' => 50,
+                        ],
+                        [
+                            'name' => '付费率',
+                            'value' => '1%',
+                        ],
+                        [
+                            'name' => 'ARPPU',
+                            'value' => 54,
+                        ],
+                    ],
+                    '2015-06-02' => [
+                        [
+                            'name' => '收入',
+                            'value' => 102,
+                        ],
+                        [
+                            'name' => '日活跃',
+                            'value' => 202,
+                        ],
+                        [
+                            'name' => '付费用户',
+                            'value' => 2,
+                        ],
+                        [
+                            'name' => '付费率',
+                            'value' => '12%',
+                        ],
+                        [
+                            'name' => 'ARPPU',
+                            'value' => 52,
+                        ],
+                    ]
+                ],
+                'remain' => [
+                    '2015-06-01' => [
+
+                        [
+                            'name' => '次日留存',
+                            'value' => '50%',
+                        ],
+                        [
+                            'name' => '3日留存',
+                            'value' => '40%',
+                        ],
+                        [
+                            'name' => '4日留存',
+                            'value' => '30%',
+                        ],
+                    ],
+
+                    '2015-06-02' => [
+
+                        [
+                            'name' => '次日留存',
+                            'value' => '48%',
+                        ],
+                        [
+                            'name' => '3日留存',
+                            'value' => '25%',
+                        ],
+                        [
+                            'name' => '4日留存',
+                            'value' => '10%',
+                        ],
+                    ],
+
+                ],
+            ],
+            [
+                'base' => [
+                    '2015-06-01' => [
+                        [
+                            'name' => '收入',
+                            'value' => 100,
+                        ],
+                        [
+                            'name' => '日活跃',
+                            'value' => 200,
+                        ],
+                        [
+                            'name' => '付费用户',
+                            'value' => 50,
+                        ],
+                        [
+                            'name' => '付费率',
+                            'value' => '1%',
+                        ],
+                        [
+                            'name' => 'ARPPU',
+                            'value' => 54,
+                        ],
+                    ],
+                    '2015-06-02' => [
+                        [
+                            'name' => '收入',
+                            'value' => 102,
+                        ],
+                        [
+                            'name' => '日活跃',
+                            'value' => 202,
+                        ],
+                        [
+                            'name' => '付费用户',
+                            'value' => 2,
+                        ],
+                        [
+                            'name' => '付费率',
+                            'value' => '12%',
+                        ],
+                        [
+                            'name' => 'ARPPU',
+                            'value' => 52,
+                        ],
+                    ],
+                    '2015-06-03' => [
+                        [
+                            'name' => '收入',
+                            'value' => 102,
+                        ],
+                        [
+                            'name' => '日活跃',
+                            'value' => 202,
+                        ],
+                        [
+                            'name' => '付费用户',
+                            'value' => 2,
+                        ],
+                        [
+                            'name' => '付费率',
+                            'value' => '12%',
+                        ],
+                        [
+                            'name' => 'ARPPU',
+                            'value' => 52,
+                        ],
+                    ]
+                ],
+                'remain' => [
+                    '2015-06-01' => [
+
+                        [
+                            'name' => '次日留存',
+                            'value' => '50%',
+                        ],
+                        [
+                            'name' => '3日留存',
+                            'value' => '40%',
+                        ],
+                        [
+                            'name' => '4日留存',
+                            'value' => '30%',
+                        ],
+                    ],
+
+                    '2015-06-02' => [
+
+                        [
+                            'name' => '次日留存',
+                            'value' => '48%',
+                        ],
+                        [
+                            'name' => '3日留存',
+                            'value' => '25%',
+                        ],
+                        [
+                            'name' => '4日留存',
+                            'value' => '10%',
+                        ],
+                    ],
+                    '2015-06-03' => [
+                        [
+                            'name' => '收入',
+                            'value' => 102,
+                        ],
+                        [
+                            'name' => '日活跃',
+                            'value' => 202,
+                        ],
+                        [
+                            'name' => '付费用户',
+                            'value' => 2,
+                        ],
+                        [
+                            'name' => '付费率',
+                            'value' => '12%',
+                        ],
+                        [
+                            'name' => 'ARPPU',
+                            'value' => 52,
+                        ],
+                    ]
+
+                ],
+            ]
+        ];
+        $time = $_POST['stime'];
+        $k = date('d', $time) % 2;
+        $data = $datas[$k];
+        echo json_encode($data);
+    }
 }
 
 ?>
--- statics/js/my-app.js
+++ statics/js/my-app.js
@@ -1,6 +1,8 @@
 // Initialize app
 var myApp = new Framework7({
     template7Pages: true,
+    //pushState:true,
+    hideToolbarOnPageScroll:true,
     allowDuplicateUrls:true
 });
 
@@ -14,39 +16,91 @@ var mainView = myApp.addView('.view-main', {
 });
 
 
+/*======================================================
+ ************   模板   ************
+ ======================================================*/
+var summaryTpl = $$('#summary-template').html();
+var compSummaryTpl = Template7.compile(summaryTpl);
+
+var dataTpl = $$('#detail-template').html();
+var compDataTpl = Template7.compile(dataTpl);
+
+var dataTpl2 = $$('#detail-template2').html();
+var compDataTpl2 = Template7.compile(dataTpl2);
+
+var loadingTml = '<p style="text-align:center;"><span class="preloader"></span></p>';
+
+/*======================================================
+ ************   首页   ************
+ ======================================================*/
 // 下拉刷新页面
 var freshContent = $$('.pull-to-refresh-content');
-
 // 添加'refresh'监听器
 freshContent.on('refresh', function (e) {
-    // 模拟2s的加载过程
+    // 模拟1s的加载过程
     setTimeout(function () {
         // 加载完毕需要重置
-        myApp.pullToRefreshDone();
-    }, 2000);
+        get_summary({}, myApp.pullToRefreshDone);
+    }, 500);
 });
 
+function get_summary(params, callback) {
+    $$('#total-box').html(loadingTml);
+    $$('#summary-box').html(loadingTml);
+    $$.post('datas.php?type=all', params, function(data, status, xhr){
+        if(status == 200)
+        {
+            var ret = JSON.parse(data);
+            var _html = compSummaryTpl({
+                data : ret.detail
+            });
+            $$('#summary-box').html(_html);
+
+            var _total_html = compDataTpl({
+                data : ret.total
+            });
+            $$('#total-box').html(_total_html);
+        }
+        else
+        {
+            myApp.alert('数据获取失败', '提示');
+        }
+        if(callback) callback();
+    });
+}
+myApp.onPageInit('index', function(page){
+    get_summary();
+}).trigger();
 
 
 
 
-var dataTpl = '{{> "dataTpl"}}';
-Template7.registerPartial(
-    'dataTpl',
-    '<div class="list-block">' +
-    '<ul>' +
-    '{{#each data}}' +
-    '<li class="item-content">' +
-    '<div class="item-inner">' +
-    '<div class="item-title">{{name}}</div>' +
-    '<div class="item-after">{{value}}</div>' +
-    '</div>' +
-    '</li>' +
-    '{{/each}}' +
-    '</ul>' +
-    '</div>'
-);
-var compDataTpl = Template7.compile(dataTpl);
+/*======================================================
+ ************   详细页面   ************
+ ======================================================*/
+
+//获取详细数据页面
+function get_detail(params)  {
+    $$.post('datas.php?type=detail&game=', params, function(data, status, xhr){
+        if(status == 200)
+        {
+            var ret = JSON.parse(data);
+            var baseData = compDataTpl({
+                data: ret.base
+            });
+            $$('#tab1').html(baseData);
+
+            var liucunData = compDataTpl({
+                data: ret.remain
+            });
+            $$('#tab2').html(liucunData);
+        }
+        else
+        {
+            myApp.alert('数据获取失败', '提示');
+        }
+    });
+}
 
 myApp.onPageInit('detail', function(page){
     myApp.calendar({
@@ -58,29 +112,69 @@ myApp.onPageInit('detail', function(page){
         onClose: function(p){
             if(p.changed)
             {
-                $$.post('datas.php', {
-                    'datetime' : p.value[0]/1000
-                }, function(data, status, xhr){
-                    if(status == 200)
-                    {
-                        var ret = JSON.parse(data);
-                        var baseData = compDataTpl({
-                            data: ret.base
-                        });
-                        $$('#tab1').html(baseData);
-
-                        var liucunData = compDataTpl({
-                            data: ret.remain
-                        });
-                        $$('#tab2').html(liucunData);
-                    }
-                    else
-                    {
-                        myApp.alert('数据获取失败', '提示');
-                    }
-                });
+                p.changed = false;
+                var params = {
+                    datetime : p.value[0]/1000
+                };
+                get_detail(params);
             }
-            p.changed = false;
+        }
+    });
+
+});
+
+
+
+
+/*======================================================
+ ************   详细页面2   ************
+ ======================================================*/
+function get_detail2(params)  {
+    $$.post('datas.php?type=detail2&game=', params, function(data, status, xhr){
+        if(status == 200)
+        {
+            var ret = JSON.parse(data);
+            var baseData = compDataTpl2({
+                data: ret.base
+            });
+            $$('#tab1').html(baseData);
+
+            var liucunData = compDataTpl2({
+                data: ret.remain
+            });
+            $$('#tab2').html(liucunData);
+        }
+        else
+        {
+            myApp.alert('数据获取失败', '提示');
+        }
+    });
+}
+myApp.onPageInit('detail2', function(page){
+    console.log('detail2');
+    myApp.calendar({
+        multiple:true,
+        monthNames: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+        input: '.calendar-default',
+        onChange: function(p, values, displayValues){
+            p.changed = true;
+        },
+        onClose: function(p){
+            if(p.changed)
+            {
+                p.changed = false;
+
+                if(p.value.length > 2 || p.value.length < 1)
+                {
+                    myApp.alert('请选择一个或者两个日期', '提示');
+                    return false;
+                }
+                var params = {
+                    stime : p.value[0]/1000,
+                    etime : p.value[1]/1000
+                };
+                get_detail2(params);
+            }
         }
     });
 
